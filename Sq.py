d9710523885f43e1b0448d718d7f9e9a8df84f28
João Vitor <71740709+Joao24820@users.noreply.github.com>
2022-01-11 20:44:11 -0300

    Create Sq.py

---
+++ Sq.py
@@ -0,0 +1,11 @@
+n1 = float(input('Informe o primeiro numero: '))
+n2 = float(input('Informe o segundo numero: '))
+n3 = float(input('Informe o terceiro numero: '))
+
+n1s = n1**4
+n2s = n2**4
+n3s = n3**4
+
+print('O resultado é {}'.format(n1s))
+print('O resultado é {}'.format(n2s))
+print('O resultado é {}'.format(n3s))
